906e40dfdffe1c298b00b2200473ac783d1052ee
ben <bennyfreemantle@protonmail.com>
2022-07-12 02:15:10 +0100

    complete overhaul and rewrite, took some inspo from similar projects

--- app.js
+++ app.js
@@ -1,88 +1,165 @@
+// Buttons
 const numberBtns = document.querySelectorAll(".didgit-button");
-const divideBtn = document.querySelector(".divide-button");
-const multiplyBtn = document.querySelector(".multiply-button");
-const subtractBtn = document.querySelector(".subtract-button");
-const addBtn = document.querySelector(".add-button");
-
+const operateBtns = document.querySelectorAll(".operate-button");
 const equalsBtn = document.querySelector(".equals-button");
-const deleteBtn = document.querySelector(".delete-button");
-const numberDisplay = document.querySelector(".calc-display > p");
+const clearBtn = document.querySelector(".clear-button");
+
+// Text UI
+const lastOperationScreen = document.querySelector("#lastOperationScreen");
+const currentOperationScreen = document.querySelector(
+  "#currentOperationScreen"
+);
 
 // Default settings
-numberDisplay.textContent = "0";
-let chosenOperation = null;
-let tempDisplayValue = "";
-let firstValue = "";
-let valueArr = [];
+lastOperationScreen.textContent = "";
+currentOperationScreen.textContent = "0";
+let firstOperand = "";
+let secondOperand = "";
+let resetScreen = false;
+let currentOperator = null;
+let finalValue = false;
 
-const operate = (operator, num1, num2) => {
-  return operator(num1, num2);
-};
+clearBtn.addEventListener("click", clear);
+equalsBtn.addEventListener("click", () => {
+  calculate();
+  finalValue = true;
+});
 
-const add = (num1, num2) => num1 + num2;
-const subtract = (num1, num2) => num1 - num2;
-const multiply = (num1, num2) => num1 * num2;
-const divide = (num1, num2) => num1 / num2;
+numberBtns.forEach((num) => {
+  num.addEventListener("click", () => {
+    if (currentOperationScreen.textContent === "0" || resetScreen) {
+      resettingScreen();
+    }
 
-// Event listener for all number buttons
-numberBtns.forEach((number) => {
-  number.addEventListener("click", () => {
-    tempDisplayValue += number.textContent;
-    valueArr.push(+number.textContent);
-    numberDisplay.textContent = tempDisplayValue;
+    if (finalValue) {
+      finalValue = false;
+      resettingScreen();
+    }
+
+    // dont allow double decimal point
+    if (
+      currentOperationScreen.textContent.charAt(
+        currentOperationScreen.textContent.length - 1
+      ) === "."
+    ) {
+      if (num.textContent === ".") {
+        return;
+      }
+    }
+
+    // dont allow anymore decimal points if it already contains one
+    if (currentOperationScreen.textContent.includes(".")) {
+      if (num.textContent === ".") {
+        return;
+      }
+    }
+
+    // if first character is decimal point, pad with zero
+    if (currentOperationScreen.textContent.length - 1 == -1) {
+      if (num.textContent === ".") {
+        console.log("gotcha");
+        currentOperationScreen.textContent = "0";
+      }
+    }
+
+    currentOperationScreen.textContent += num.textContent;
   });
 });
 
-const calculate = () => {
-  let result = operate(chosenOperation, +firstValue, +tempDisplayValue);
-  firstValue = "";
-  numberDisplay.textContent = result;
-  tempDisplayValue = result;
-};
+// Sets operator when operator buttons are clicked
+operateBtns.forEach((operator) => {
+  operator.addEventListener("click", () => setOperation(operator.textContent));
+});
 
-// Event listeners for operation buttons
-addBtn.addEventListener("click", () => {
-  if (!firstValue == "" && !tempDisplayValue == "") {
-    calculate();
+// // append the new number to the previous
+// function appendNumber(num) {
+//   if (currentOperationScreen.textContent === "0" || resetScreen) {
+//     resettingScreen();
+//   }
+
+//   if (finalValue) {
+//     finalValue = false;
+//     resettingScreen();
+//   }
+
+//   if (
+//     currentOperationScreen.textContent.charAt(
+//       currentOperationScreen.textContent.length - 1
+//     ) === "."
+//   ) {
+//     if (num.textContent === ".") {
+//       console.log("booya");
+//     }
+//   }
+
+//   currentOperationScreen.textContent += num;
+// }
+
+// clears the current operation screen
+function resettingScreen() {
+  currentOperationScreen.textContent = "";
+  resetScreen = false;
+}
+
+// Clears all saved values back to default
+function clear() {
+  currentOperationScreen.textContent = "0";
+  lastOperationScreen.textContent = "";
+  firstOperand = "";
+  secondOperand = "";
+  currentOperator = null;
+}
+
+function setOperation(operator) {
+  if (currentOperator !== null) calculate();
+  firstOperand = currentOperationScreen.textContent;
+  currentOperator = operator;
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperator}`;
+  resetScreen = true;
+  console.log("opperator set " + currentOperator);
+}
+
+function calculate() {
+  console.log(currentOperator);
+  if (currentOperator === null || resetScreen) return;
+  if (currentOperator === "÷" && currentOperationScreen.textContent === "0") {
+    alert("You can't divide by 0!");
+    clear();
+    return;
   }
-  firstValue = tempDisplayValue;
-  tempDisplayValue = "";
-  chosenOperation = add;
-});
+  console.log(`${Number(firstOperand)} ${currentOperator} ${+secondOperand} =`);
+  secondOperand = currentOperationScreen.textContent;
+  currentOperationScreen.textContent = roundNum(
+    operate(currentOperator, firstOperand, secondOperand)
+  );
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperator} ${secondOperand} =`;
+  currentOperator = null;
+}
 
-subtractBtn.addEventListener("click", () => {
-  if (!firstValue == "" && !tempDisplayValue == "") {
-    calculate();
+function roundNum(num) {
+  return Math.round(num * 1000) / 1000;
+}
+
+// Calculating logic
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "x":
+      return multiply(a, b);
+    case "÷":
+      if (b === 0) return null;
+      else return divide(a, b);
+    default:
+      return null;
   }
-  firstValue = tempDisplayValue;
-  tempDisplayValue = "";
-  chosenOperation = subtract;
-});
-
-multiplyBtn.addEventListener("click", () => {
-  if (!firstValue == "" && !tempDisplayValue == "") {
-    calculate();
-  }
-  firstValue = tempDisplayValue;
-  tempDisplayValue = "";
-  chosenOperation = multiply;
-});
-
-divideBtn.addEventListener("click", () => {
-  if (!firstValue == "" && !tempDisplayValue == "") {
-    calculate();
-  }
-  firstValue = tempDisplayValue;
-  tempDisplayValue = "";
-  chosenOperation = divide;
-});
-
-deleteBtn.addEventListener("click", () => {
-  firstValue = "";
-  tempDisplayValue = "";
-  numberDisplay.textContent = "0";
-});
-
-equalsBtn.addEventListener("click", () => {
-  calculate();
-});
+}
